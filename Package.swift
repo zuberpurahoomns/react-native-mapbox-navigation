bbf0c5363055a211c34d83e4e6e44e38603ff24d
zuber.purahoo <zuber.purahoo@mns.mu>
2024-10-23 14:20:40 +0400

    feat(add): add package.swift
    Add Package.swift to include MapboxNavigationCore

--- Package.swift
+++ Package.swift
@@ -1,34 +1,26 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "react-native-mapbox-navigation",
-    platforms: [
-        .iOS(.v14)
-    ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "react-native-mapbox-navigation",
-            targets: ["react-native-mapbox-navigation"]
-        ),
+            targets: ["react-native-mapbox-navigation"]),
     ],
     dependencies: [
-        .package(
-            name: "MapboxNavigation",
-            url: "https://github.com/mapbox/mapbox-navigation-ios.git",
-            .upToNextMajor(from: "3.4.0")
-        ),
+        .package(url: "https://github.com/mapbox/mapbox-navigation-ios.git", from: "3.4.0")
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "react-native-mapbox-navigation",
-            dependencies: [
-                .product(name: "MapboxNavigation", package: "MapboxNavigation"),
-                .product(name: "MapboxCoreNavigation", package: "MapboxNavigation")
-            ],
-            path: "ios",
-            exclude: [],
-            sources: ["."],
-            publicHeadersPath: ""
-        ),
+            name: "react-native-mapbox-navigation"),
+        .testTarget(
+            name: "react-native-mapbox-navigationTests",
+            dependencies: ["react-native-mapbox-navigation"]),
     ]
 )
