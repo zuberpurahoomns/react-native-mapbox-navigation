16425003025767de54c73574805bea0e58c80dfb
zuber.purahoo <zuber.purahoo@mns.mu>
2024-10-23 14:20:26 +0400

    feat(add): add package.swift
    Add Package.swift to include MapboxNavigationCore

---
+++ Tests/react-native-mapbox-navigationTests/react_native_mapbox_navigationTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import react_native_mapbox_navigation
+
+final class react_native_mapbox_navigationTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
